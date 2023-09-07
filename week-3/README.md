f6aa616b1eea0d287230de85d65636315ec2566d
Rudra <rudra.behera50@gmail.com>
2023-09-07 22:26:53 +0530

    Working on todo app
    Signed-off-by: Rudra <rudra.behera50@gmail.com>

--- week-3/README.md
+++ week-3/README.md
@@ -1,6 +1,6 @@
 <h1 style="color:yellow"><center> Databases, Storage and basic Frontend 🧾</center></h1>
 
-foudnation for databases
+foundation for databases
 connecting frontend to backend
 AJAX
 cors
@@ -39,4 +39,42 @@ app.get("/todos", function (req, res) {
 app.get("/todos", (req, res) => {
   res.json(todos);
 });
+```
+
+### CORS:-
+
+> Cross-Origin Resource Sharing (CORS) is a mechanism which aims to allow requests made on behalf of you and at the same time block some requests made by rogue JS and is triggered whenever you are making an HTTP request to:
+
+    -> a different domain (eg. site at example.com calls api.com)
+    -> a different sub domain (eg. site at example.com calls api.example.com)
+    -> a different port (eg. site at example.com calls example.com:3001)
+    -> a different protocol (eg. site at https://example.com calls http://example.com)
+
+- If your browser tries to make a “non simple” request (eg. an request that includes cookies, or which Content-type is other than application/x-ww-form-urlencoded, multipart/form-data or text-plain) an mechanism called **preflight** will be used and an OPTIONS request will be sent to the server.
+
+#### Access-Control-Allow-Origin
+
+- This header is meant to be returned by the server, and indicate what client-domains are allowed to access its resources. The value can be:
+
+      -> * — allow any domain
+      -> a fully qualified domain name (eg. https://example.com)
+
+**Note**:- If you require the client to pass authentication headers (e.g. cookies) the value can not be \* — it must be a fully qualified domain!
+
+#### How to fix the CORS “error”?
+
+- You have to understand that the CORS behavior is not an error — it’s a mechanism that’s working as expected in order to protect your users, you, or the site you’re calling.
+
+1. **Case I**: -
+
+   > I’m developing the frontend and have control of or know the person developing the backend
+
+   > **Solution**: - This is the best case scenario — you should be able to implement the proper CORS response on the server which you’re calling. If the API is using express for node you can use the simple cors package. If you want to make your site properly secure, consider using a whitelist for the Access-Control-Allow-Origin header.
+
+2. **Case II**:- I’m developing the frontend but have no control of the backend now, I need a temporary solution
+
+   > This is the second-best scenario, because it’s just the A one, but with some time constrains. To temporary fix the issue you can make your browser ignore CORS mechanism — for example use the ACAO Chrome extension or by disabling it completely by running Chrome with the following flags:
+
+```sh
+chrome --disable-web-security --user-data-dir
 ```
---
+++ week-3/todoApp/index.js
@@ -0,0 +1,120 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const fs = require("fs");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+
+// Enable CORS for all routes
+app.use(cors());
+
+app.use(bodyParser.json());
+
+function findIndex(arr, id) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].id === id) return i;
+  }
+  return -1;
+}
+
+function removeAtIndex(arr, index) {
+  let newArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (i !== index) newArray.push(arr[i]);
+  }
+  return newArray;
+}
+
+app.get("/todos", (req, res) => {
+  fs.readFile("todos.json", "utf-8", (error, data) => {
+    if (error) throw error;
+
+    res.json(JSON.parse(data));
+  });
+});
+
+app.get("/todos/:id", (req, res) => {
+  fs.readFile("todos.json", "utf-8", (err, data) => {
+    if (err) throw err;
+    const todos = JSON.parse(data);
+    const todoIndex = findIndex(todos, parseInt(req.params.id));
+
+    if (todoIndex === -1) {
+      res.status(404).send();
+    } else {
+      res.json(todos[todoIndex]);
+    }
+  });
+});
+
+app.post("/todos", (req, res) => {
+  const todo = {
+    id: Math.floor(Math.random() * 1000000),
+    title: req.body.title,
+    description: req.body.description,
+  };
+
+  fs.readFile("todos.json", "utf-8", (err, data) => {
+    if (err) throw err;
+
+    const todos = JSON.parse(data);
+    todos.push(todo);
+    fs.writeFile("todos.json", JSON.stringify(todos), (err) => {
+      if (err) throw err;
+      res.status(201).json(todo);
+    });
+  });
+});
+
+app.put("/todos/:id", (req, res) => {
+  fs.readFile("todos.json", "utf8", (err, data) => {
+    if (err) throw err;
+    const todos = JSON.parse(data);
+    const todoIndex = findIndex(todos, parseInt(req.params.id));
+    if (todoIndex === -1) {
+      res.status(404).send();
+    } else {
+      const updatedTodo = {
+        id: todos[todoIndex].id,
+        title: req.body.title,
+        description: req.body.description,
+      };
+      todos[todoIndex] = updatedTodo;
+      fs.writeFile("todos.json", JSON.stringify(todos), (err) => {
+        if (err) throw err;
+        res.status(200).json(updatedTodo);
+      });
+    }
+  });
+});
+
+app.delete("/todos/:id", (req, res) => {
+  fs.readFile("todos.json", "utf8", (err, data) => {
+    if (err) throw err;
+    const todos = JSON.parse(data);
+    const todoIndex = findIndex(todos, parseInt(req.params.id));
+    if (todoIndex === -1) {
+      res.status(404).send();
+    } else {
+      todos = removeAtIndex(todos, todoIndex);
+      fs.writeFile("todos.json", JSON.stringify(todos), (err) => {
+        if (err) throw err;
+        res.status(200).send();
+      });
+    }
+  });
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+// for all other routes, return 404
+app.use((req, res, next) => {
+  res.status(404).send();
+});
+
+app.listen(3000, () => {
+  console.log(`Listening on port 3000`);
+});
